d7ce60ddcab56a93ced28cb2dbd630fd76af7f31
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2023-11-15 10:34:49 -0500

    add config mailer and Did you forget your password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,18 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            ENV['GMAIL_USERNAME'],
+  password:             ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true 
+}
 
   config.enable_reloading = true
 
